5765697329f2cbcbc8139d3c5669d7569874cf1b
OanaSabinaIancu <109290330+OanaSabinaIancu@users.noreply.github.com>
2024-01-07 08:44:36 +0200

    plane rotation view added

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -6,6 +6,9 @@ public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     private Vector3 offset = new Vector3(0, 4, -8);
+    private Vector3 rotationPlane = new Vector3(0, 90, 0);
+    private float verticalInput;
+    public float speed = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,10 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        verticalInput = Input.GetAxis("Vertical");
         //setting a camera offset on the plane
         transform.position = plane.transform.position + offset;
+        transform.Rotate(offset, verticalInput * Time.deltaTime * speed);
     }
 }
